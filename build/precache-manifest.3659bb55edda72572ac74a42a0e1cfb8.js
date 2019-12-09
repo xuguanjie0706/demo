5d5b5deb74c85d0d5de4f40286a691f3721c962d
xu0105 <215790428@qq.com>
2019-12-09 13:22:58 +0800

    fda

---
+++ build/precache-manifest.3659bb55edda72572ac74a42a0e1cfb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ed869b34d268fd8b1d44bb52fb4823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d806c56bdb3d50dcdccb",
+    "url": "/static/css/2.05d91f78.chunk.css"
+  },
+  {
+    "revision": "6852d7ebc47fb5bfe46d",
+    "url": "/static/css/main.d80c2c6e.chunk.css"
+  },
+  {
+    "revision": "d806c56bdb3d50dcdccb",
+    "url": "/static/js/2.ca55e308.chunk.js"
+  },
+  {
+    "revision": "6852d7ebc47fb5bfe46d",
+    "url": "/static/js/main.ca03b144.chunk.js"
+  },
+  {
+    "revision": "3a571d1ca74dc6a22b67",
+    "url": "/static/js/runtime-main.1d9037dc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
